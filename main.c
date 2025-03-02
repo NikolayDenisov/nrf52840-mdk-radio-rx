b6fdec830f268c92d5fd08978510a2680cd0f736
Николай Денисов <nickdenisov@protonmail.com>
2025-03-02 13:04:09 +0500

    Create: working rx

--- main.c
+++ main.c
@@ -1,89 +1,140 @@
-#include "nrf52840.h"
-#include "nrf52840_bitfields.h"
+#include "app_timer.h"
+#include "boards.h"
+#include "bsp.h"
+#include "nordic_common.h"
+#include "nrf_delay.h"
+#include "nrf_error.h"
+#include "nrf_gpio.h"
+#include "radio_config.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 
-// Настройка радиоприемника nRF52840 для приема данных
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+#include "nrf_radio.h"
+
+static uint32_t packet; /**< Packet to transmit. */
+
+void clock_initialization() {
+  /* Start 16 MHz crystal oscillator */
+  NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_HFCLKSTART = 1;
+
+  /* Wait for the external oscillator to start up */
+  while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) {
+    // Do nothing.
+  }
+
+  /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
+  NRF_CLOCK->LFCLKSRC = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+  NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_LFCLKSTART = 1;
+
+  while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) {
+    // Do nothing.
+  }
+}
+
+uint32_t read_packet() {
+  uint32_t result = 0;
+
+  NRF_RADIO->EVENTS_READY = 0;
+  // Enable radio and wait for ready
+  NRF_RADIO->TASKS_RXEN = 1;
+
+  while (!NRF_RADIO->EVENTS_READY) {
+    // wait
+  }
+  NRF_RADIO->EVENTS_END = 0;
+  // Start listening and wait for address received event
+  NRF_RADIO->TASKS_START = 1;
+
+  // Wait for end of packet or buttons state changed
+  while (!NRF_RADIO->EVENTS_END) {
+    // wait
+  }
+  result = packet;
+  NRF_RADIO->EVENTS_DISABLED = 0;
+  // Disable radio
+  NRF_RADIO->TASKS_DISABLE = 1;
+  while (!NRF_RADIO->EVENTS_DISABLED) {
+    // wait
+  }
+  return result;
+}
+
 void radio_init() {
-  // Включение радиомодуля
-  NRF_RADIO->POWER = RADIO_POWER_POWER_Enabled;
+  // Radio config
+  NRF_RADIO->TXPOWER =
+      (RADIO_TXPOWER_TXPOWER_0dBm << RADIO_TXPOWER_TXPOWER_Pos);
+  NRF_RADIO->FREQUENCY = 7UL; // Frequency bin 7, 2407MHz
+  NRF_RADIO->MODE = (RADIO_MODE_MODE_Nrf_1Mbit << RADIO_MODE_MODE_Pos);
 
-  // Настройка базовых параметров радио
-  NRF_RADIO->MODE = RADIO_MODE_MODE_Ble_1Mbit; // Используем BLE с пропускной
-                                               // способностью 1 Mbit
+  // Radio address config
+  NRF_RADIO->PREFIX0 = 0xC3C28303;
+  NRF_RADIO->PREFIX1 = 0xE3630023;
 
-  // Настройка адреса приемника
-  NRF_RADIO->PREFIX0 = 0x8E;     // Префикс адреса, например 0x8E
-  NRF_RADIO->BASE0 = 0x89ABCDEF; // Базовый адрес, например 0x89ABCDEF
-  NRF_RADIO->TXADDRESS = 0;      // Используем адрес 0 для передачи
-  NRF_RADIO->RXADDRESSES =
-      RADIO_RXADDRESSES_ADDR0_Enabled; // Включаем адрес 0 для приема
+  NRF_RADIO->BASE0 = 0x80C4A2E6UL;
+  NRF_RADIO->BASE1 = 0x91D5B3F7UL;
 
-  // Настройка каналов и частоты
-  NRF_RADIO->FREQUENCY = 2; // Настраиваем частоту на 2402 MHz (канал 2)
-  NRF_RADIO->DATAWHITEIV =
-      37; // Инициализация переменной дециметрии данных на канал 37
+  NRF_RADIO->TXADDRESS =
+      0x00UL; // Set device address 0 to use when transmitting
+  NRF_RADIO->RXADDRESSES = 0x01UL; // Enable device address 0 to use to select
+                                   // which addresses to receive
 
-  // Настройка пакетного конфигурации
+  // Packet configuration
   NRF_RADIO->PCNF0 =
-      (1 << RADIO_PCNF0_S0LEN_Pos) |
-      (8 << RADIO_PCNF0_LFLEN_Pos); // Длина поля S0 и поля длины данных
+      (0UL << RADIO_PCNF0_S1LEN_Pos) | (0UL << RADIO_PCNF0_S0LEN_Pos) |
+      (0UL
+       << RADIO_PCNF0_LFLEN_Pos); // lint !e845 "The right argument to operator
+                                  // '|' is certain to be 0"
+
+  // Packet configuration
   NRF_RADIO->PCNF1 =
       (RADIO_PCNF1_WHITEEN_Disabled << RADIO_PCNF1_WHITEEN_Pos) |
-      (RADIO_PCNF1_ENDIAN_Little << RADIO_PCNF1_ENDIAN_Pos) |
-      (24 << RADIO_PCNF1_MAXLEN_Pos); // Макс. длина пакета данных 24 байта
-
-  // Настройка адреса начала приема
-  NRF_RADIO->TASKS_RXEN = 1; // Включаем режим приема
+      (RADIO_PCNF1_ENDIAN_Big << RADIO_PCNF1_ENDIAN_Pos) |
+      (PACKET_BASE_ADDRESS_LENGTH << RADIO_PCNF1_BALEN_Pos) |
+      (PACKET_STATIC_LENGTH << RADIO_PCNF1_STATLEN_Pos) |
+      (PACKET_PAYLOAD_MAXSIZE
+       << RADIO_PCNF1_MAXLEN_Pos); // lint !e845 "The right argument to operator
+                                   // '|' is certain to be 0"
 }
-void check_radio_state() {
-  uint32_t state = NRF_RADIO->STATE;
-  __asm__("NOP");
 
-  // switch (state) {
-  // case RADIO_STATE_STATE_RxIdle:
-  //   printf("Радиомодуль в режиме ожидания приема (RXIDLE).\n");
-  //   break;
-  // case RADIO_STATE_STATE_Rx:
-  //   printf("Радиомодуль активно принимает пакет (RXACT).\n");
-  //   break;
-  // case RADIO_STATE_STATE_TxIdle:
-  //   printf("Радиомодуль в режиме ожидания передачи (TXIDLE).\n");
-  //   break;
-  // case RADIO_STATE_STATE_Tx:
-  //   printf("Радиомодуль активно передает пакет (TXACT).\n");
-  //   break;
-  // default:
-  //   printf("Неизвестное состояние радиомодуля.\n");
-  //   break;
-  // }
-}
-// Основная функция
 int main(void) {
-  // Инициализация радиомодуля
+  NRF_LOG_INFO("Denisov");
+  uint32_t err_code = NRF_SUCCESS;
+
+  clock_initialization();
+
+  err_code = app_timer_init();
+  APP_ERROR_CHECK(err_code);
+
+  err_code = NRF_LOG_INIT(NULL);
+  APP_ERROR_CHECK(err_code);
+  NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+  err_code = bsp_init(BSP_INIT_LEDS, NULL);
+  APP_ERROR_CHECK(err_code);
+
+  // Set radio configuration parameters
   radio_init();
+  NRF_RADIO->PACKETPTR = (uint32_t)&packet;
 
-  // Основной цикл программы
-  while (1) {
-    if (NRF_RADIO->EVENTS_DEVMISS) {
-      NRF_RADIO->EVENTS_DEVMISS = 0; // Сбрасываем событие
-      NRF_RADIO->TASKS_DISABLE =
-          1; // Отключаем приемник, так как адрес не совпал
-    } else {
-      // Адрес совпал — продолжаем обработку пакета
-    }
-    // Ожидание приема данных
-    if (NRF_RADIO->EVENTS_END) {
-      // Обработка полученных данных
-      uint8_t received_data[24];
-      memcpy(received_data, (const uint8_t *)NRF_RADIO->PACKETPTR, 24);
+  err_code = bsp_indication_set(BSP_INDICATE_USER_STATE_OFF);
+  NRF_LOG_INFO("Radio receiver example started.");
+  NRF_LOG_INFO("Wait for first packet");
+  APP_ERROR_CHECK(err_code);
+  NRF_LOG_FLUSH();
 
-      // Сброс событий радио
-      NRF_RADIO->EVENTS_END = 0;
-      NRF_RADIO->TASKS_START = 1; // Повторный запуск радио для приема данных
-    }
-    check_radio_state();
+  while (true) {
+    uint32_t received = read_packet();
+
+    NRF_LOG_INFO("Packet was received");
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_INFO("The contents of the package is %u", (unsigned int)received);
+    NRF_LOG_FLUSH();
   }
 }
