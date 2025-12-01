66e177348f658b594c0dfb09ce96e082d4b6f94b
Николай Денисов <nickdenisov@protonmail.com>
2025-12-01 23:58:30 +0500

    Add(feature): README.md

--- src/radio.c
+++ src/radio.c
@@ -4,6 +4,26 @@
 
 static uint32_t packet;
 
+void clock_initialization() {
+  /* Start 16 MHz crystal oscillator */
+  NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_HFCLKSTART = 1;
+
+  /* Wait for the external oscillator to start up */
+  while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) {
+    // Do nothing.
+  }
+
+  /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
+  NRF_CLOCK->LFCLKSRC = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+  NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_LFCLKSTART = 1;
+
+  while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) {
+    // Do nothing.
+  }
+}
+
 void radio_init() {
   NRF_RADIO->TXPOWER = 0UL;
   NRF_RADIO->FREQUENCY = 7UL;
